8311da9818b1f2ec4d6168dd0ed516352ea6f2dd
Mario Wenzel <maweki@gmail.com>
2018-12-12 17:28:16 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+mvn clean install -DskipTests
+pwd
+cd packaging/standalone/target
+pwd
+tar -vxzf neo4j-community-3.5.0-SNAPSHOT-unix.tar.gz
+cp -vr neo4j-community-3.5.0-SNAPSHOT ${PREFIX}/neo4j
+pwd
+
+echo ${PREFIX}
+mkdir -vp ${PREFIX}/bin
+ln -s ${PREFIX}/neo4j/bin/neo4j ${PREFIX}/bin/neo4j
+ln -s ${PREFIX}/neo4j/bin/neo4j-admin ${PREFIX}/bin/neo4j-admin
+ln -s ${PREFIX}/neo4j/bin/neo4j-import ${PREFIX}/bin/neo4j-import
